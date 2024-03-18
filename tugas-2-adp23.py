e1ba4cd80371a482ac1a24ef63ed2fb299c205ad
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-03-19 00:01:07 +0700

    Update tugas-2-adp23.py

--- tugas-2-adp23.py
+++ tugas-2-adp23.py
@@ -1,1 +1,20 @@
+finish = "N"
 
+while finish == "N":
+    fsal = float(input("Salary Based: "))
+    samt = float(input("Sales Value: "))
+
+    if samt > 5000:
+        comm = (samt - 5000) * 0.12
+    else:
+        comm = 0
+
+    finalSalary = fsal + comm
+    print("Total Salary is:", finalSalary)
+
+    finish = str(input("Wanna recalculate(Y/N)?: ").upper)
+
+    if finish == "Y":
+        break
+
+print("calculation was finished")
